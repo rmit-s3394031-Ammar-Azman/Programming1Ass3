ed7da8eaa91f84eedf4b40535c75d56259b35010
Cameron Matthews <s3379385@student.rmit.edu.au>
2015-05-11 12:55:29 +1000

    Test

--- EquipmentHire.java
+++ EquipmentHire.java
@@ -15,7 +15,46 @@ class EquipmentHire {
 		this.equipDescription = equipDescription;
 		this.hireFee = hireFee;
 		this.hireQty = hireQty;
-	                     }
+	 }
+	// Step 3 - Define accessors (getters) for each instance variable
+	public String hireID()
+	{
+		return hireID;
+	}
 	
+	public String hireName()
+	{
+		return hireName;
+	}
+	
+	public String equipDescription()
+	{
+		return equipDescription;
+	}
+	
+	public double hireFee()
+	{
+		return hireFee;
+	}
+	
+	public int hireQty(){
+		return hireQty;
+	}
+	public double getHireFee(int hireQty)
+	   {
+		
+	   }
+	public void printDetails()
+	{
+		System.out.printf("%s %s\n", "Hire ID:", hireID);
+	      System.out.printf("%s %s\n", "Description:", tourDescription);
+	      System.out.printf("%s $%.2f\n", "Tour Fee:", tourFee);
+	      System.out.printf("%s %s\n", "Total Bookings:", totalBookings);
+	      
+	      // call the calculateBookingFees() method to help figure out the
+	      // total fees 
+	      System.out.printf("%s $%.2f\n", "Total Booking Fees Received:",
+	                                      this.calculateBookingFees(totalBookings));
+	}
 	
 }
